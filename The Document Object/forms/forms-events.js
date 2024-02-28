a4f5ad59f78e8e1207adb47f9b9c1d47d6a97f73
Arjun Jaishi <a.jaishi7777777@gmail.com>
2024-02-28 17:41:01 +0545

    Keyboard Events - keyup

--- The Document Object/forms/forms-events.js
+++ The Document Object/forms/forms-events.js
@@ -5,6 +5,8 @@
 const form = document.querySelector('form');
 const username = document.querySelector('#username');
 const feedback = document.querySelector('#feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
 form.addEventListener('submit', e => {
     e.preventDefault(); //prevents default page reload behavior
 
@@ -13,7 +15,6 @@ form.addEventListener('submit', e => {
     //another way of getting form field's value is using dot (.) with form
     //condition is must have (name and id) given to the form field
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/;
     if(usernamePattern.test(username)){
         feedback.textContent='Valid Input';
         // feedback.style.color='black';
@@ -21,4 +22,17 @@ form.addEventListener('submit', e => {
         feedback.textContent='Username must contains upper and lower case letters between 6 to 12 characters';
         feedback.style.color='red'
     }
-})
+});
+
+username.addEventListener('keyup',e=>{
+    // console.log(e.target.value,'vv')
+    if(usernamePattern.test(e.target.value)){
+        form.username.setAttribute('class','success');
+        console.log('pass');
+    }else{
+        form.username.setAttribute('class','error');
+        console.log('failed');
+
+    }
+}); 
+

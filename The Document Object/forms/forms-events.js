3d4aa5e8476d4117c52c02cb904d13bcc0a07d4d
Arjun Jaishi <a.jaishi7777777@gmail.com>
2024-02-28 17:10:12 +0545

    Simple text regex

--- The Document Object/forms/forms-events.js
+++ The Document Object/forms/forms-events.js
@@ -4,6 +4,7 @@
 
 const form = document.querySelector('form');
 const username = document.querySelector('#username');
+const feedback = document.querySelector('#feedback');
 form.addEventListener('submit', e => {
     e.preventDefault(); //prevents default page reload behavior
 
@@ -11,5 +12,13 @@ form.addEventListener('submit', e => {
     // console.log(username.value);
     //another way of getting form field's value is using dot (.) with form
     //condition is must have (name and id) given to the form field
-    console.log(form.username.value);
+    const username = form.username.value;
+    const usernamePattern = /^[a-zA-Z]{6,12}$/;
+    if(usernamePattern.test(username)){
+        feedback.textContent='Valid Input';
+        // feedback.style.color='black';
+    }else{
+        feedback.textContent='Username must contains upper and lower case letters between 6 to 12 characters';
+        feedback.style.color='red'
+    }
 })
